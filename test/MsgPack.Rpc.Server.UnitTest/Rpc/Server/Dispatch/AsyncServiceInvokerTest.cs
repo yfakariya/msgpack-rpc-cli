95a1ea14f44f16f2492e4b908b3071fb9234cd5d
yfakariya <yfakariya@gmail.com>
2012-01-15 23:28:28 +0900

    Add unit tests

---
+++ test/MsgPack.Rpc.Server.UnitTest/Rpc/Server/Dispatch/AsyncServiceInvokerTest.cs
@@ -0,0 +1,115 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2010 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using MsgPack.Rpc.Server.Protocols;
+using MsgPack.Serialization;
+using System.Linq;
+
+namespace MsgPack.Rpc.Server.Dispatch
+{
+	/// <summary>
+	///Tests the Async Service Invoker 
+	/// </summary>
+	[TestFixture()]
+	public class AsyncServiceInvokerTest
+	{
+		[Test()]
+		public void TestInvokeAsync_Success_TaskSetSerializedReturnValue()
+		{
+			ServerRequestContext requestContext = new ServerRequestContext();
+			ServerResponseContext responseContext = new ServerResponseContext();
+			using ( var result = new Target( null, RpcErrorMessage.Success ).InvokeAsync( requestContext, responseContext ) )
+			{
+				result.Wait();
+			}
+
+			Assert.That( responseContext.GetReturnValueData(), Is.EqualTo( new byte[] { 123 } ) );
+			Assert.That( responseContext.GetErrorData(), Is.EqualTo( new byte[] { 0xC0 } ) );
+		}
+
+		[Test()]
+		public void TestInvokeAsync_FatalError_TaskSetSerializedError()
+		{
+			ServerRequestContext requestContext = new ServerRequestContext();
+			ServerResponseContext responseContext = new ServerResponseContext();
+			using ( var result = new Target( new Exception( "FAIL" ), RpcErrorMessage.Success ).InvokeAsync( requestContext, responseContext ) )
+			{
+				result.Wait();
+			}
+
+			var error = Unpacking.UnpackObject( responseContext.GetErrorData() );
+			var errorDetail = Unpacking.UnpackObject( responseContext.GetReturnValueData() );
+			Assert.That( error.Value.Equals( RpcError.CallError.Identifier ) );
+			Assert.That( errorDetail.Value.IsNil, Is.False );
+		}
+
+		[Test()]
+		public void TestInvokeAsync_MethodError_TaskSetSerializedError()
+		{
+			ServerRequestContext requestContext = new ServerRequestContext();
+			ServerResponseContext responseContext = new ServerResponseContext();
+			using ( var result = new Target( null, new RpcErrorMessage( RpcError.ArgumentError, MessagePackObject.Nil ) ).InvokeAsync( requestContext, responseContext ) )
+			{
+				result.Wait();
+			}
+
+			var error = Unpacking.UnpackObject( responseContext.GetErrorData() );
+			var errorDetail = Unpacking.UnpackObject( responseContext.GetReturnValueData() );
+			Assert.That( error.Value.Equals( RpcError.ArgumentError.Identifier ) );
+			Assert.That( errorDetail.Value.IsNil, Is.True );
+		}
+
+		private sealed class Target : AsyncServiceInvoker<int>
+		{
+			private readonly Exception _fatalError;
+			private readonly RpcErrorMessage _methodError;
+
+			public Target( Exception fatalError, RpcErrorMessage methodError )
+				: base( RpcServerConfiguration.Default, new SerializationContext(), new ServiceDescription( "Dummy", () => new object() ), typeof( object ).GetMethod( "ToString" ) )
+			{
+				this._fatalError = fatalError;
+				this._methodError = methodError;
+			}
+
+			protected override void InvokeCore( Unpacker arguments, out Task task, out RpcErrorMessage error )
+			{
+				if ( this._fatalError != null )
+				{
+					throw this._fatalError;
+				}
+
+				if ( this._methodError.IsSuccess )
+				{
+					task = Task.Factory.StartNew( () => 123 );
+					error = RpcErrorMessage.Success;
+				}
+				else
+				{
+					task = Task.Factory.StartNew( () => 1 );
+					error = this._methodError;
+				}
+			}
+		}
+
+	}
+}
---
+++ test/MsgPack.Rpc.Server.UnitTest/Rpc/Server/Dispatch/InvocationHelperTest.cs
@@ -0,0 +1,197 @@
+﻿#region -- License Terms --
+//
+// MessagePack for CLI
+//
+// Copyright (C) 2010 FUJIWARA, Yusuke
+//
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+//
+//        http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//
+#endregion -- License Terms --
+
+using System;
+using NUnit.Framework;
+
+namespace MsgPack.Rpc.Server.Dispatch
+{
+	/// <summary>
+	///Tests the Invocation Helper 
+	/// </summary>
+	[TestFixture()]
+	public class InvocationHelperTest
+	{
+		[Test]
+		public void TestFields()
+		{
+			Assert.That( InvocationHelper.HandleArgumentDeserializationExceptionMethod, Is.Not.Null );
+			Assert.That( InvocationHelper.HandleInvocationExceptionMethod, Is.Not.Null );
+		}
+
+		[Test]
+		public void TestTrace_SuccessAtLeast()
+		{
+			var oldLevel = MsgPackRpcServerDispatchTrace.Source.Switch.Level;
+			try
+			{
+				MsgPackRpcServerDispatchTrace.Source.Switch.Level = System.Diagnostics.SourceLevels.All;
+				InvocationHelper.TraceInvocationResult<object>( 1, Rpc.Protocols.MessageType.Request, 1, "TracingTest", RpcErrorMessage.Success, null );
+				InvocationHelper.TraceInvocationResult<object>( 1, Rpc.Protocols.MessageType.Request, 1, "TracingTest", new RpcErrorMessage( RpcError.RemoteRuntimeError, "Description", "DebugInformation" ), null );
+			}
+			finally
+			{
+				MsgPackRpcServerDispatchTrace.Source.Switch.Level = oldLevel;
+			}
+		}
+
+		[Test()]
+		public void TestHandleArgumentDeserializationException_NotNull_IsDebugMode_IncludesFullExceptionInfo()
+		{
+			var exception = new Exception( Guid.NewGuid().ToString() );
+			string parameterName = Guid.NewGuid().ToString();
+
+			var result = InvocationHelper.HandleArgumentDeserializationException( exception, parameterName, true );
+
+			Assert.That( result.IsSuccess, Is.False );
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.StringContaining( parameterName ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.DebugInformationKeyUtf8 ].AsString(), Is.StringContaining( exception.Message ) );
+		}
+
+		[Test()]
+		public void TestHandleArgumentDeserializationException_NotNull_IsNotDebugMode_DoesNotIncludeFullExceptionInfo()
+		{
+			var exception = new Exception( Guid.NewGuid().ToString() );
+			string parameterName = Guid.NewGuid().ToString();
+
+			var result = InvocationHelper.HandleArgumentDeserializationException( exception, parameterName, false );
+
+			Assert.That( result.IsSuccess, Is.False );
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.StringContaining( parameterName ) );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleArgumentDeserializationException_Null_IsDebugMode_DefaultString()
+		{
+			var result = InvocationHelper.HandleArgumentDeserializationException( null, null, true );
+
+			Assert.That( result.IsSuccess, Is.False );
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleArgumentDeserializationException_Null_IsNotDebugMode_DefaultString()
+		{
+			var result = InvocationHelper.HandleArgumentDeserializationException( null, null, false );
+
+			Assert.That( result.IsSuccess, Is.False );
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_ArgumentException_IsDebugMode_AsArgumentError()
+		{
+			var exception = new ArgumentException( Guid.NewGuid().ToString(), Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, true );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.StringContaining( exception.ParamName ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.DebugInformationKeyUtf8 ].AsString(), Is.StringContaining( exception.Message ) );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_ArgumentException_IsNotDebugMode_AsArgumentErrorWithoutDebugInformation()
+		{
+			var exception = new ArgumentException( Guid.NewGuid().ToString(), Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, false );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.ArgumentError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.StringContaining( exception.ParamName ) );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_RpcException_IsDebugMode_AsCorrespondingError()
+		{
+			var exception = new RpcException( RpcError.MessageTooLargeError, Guid.NewGuid().ToString(), Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, true );
+
+			Assert.That( result.Error, Is.EqualTo( exception.RpcError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.StringContaining( exception.Message ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.DebugInformationKeyUtf8 ].AsString(), Is.StringContaining( exception.DebugInformation ) );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_RpcException_IsDebugMode_AsCorrespondingErrorWithoutDebugInformation()
+		{
+			var exception = new RpcException( RpcError.MessageTooLargeError, Guid.NewGuid().ToString(), Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, false );
+
+			Assert.That( result.Error, Is.EqualTo( exception.RpcError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.No.Empty.And.Not.StringContaining( exception.Message ).And.Not.StringContaining( exception.DebugInformation ) );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_OtherException_IsDebugMode_AsRemoteRuntimeError()
+		{
+			var exception = new Exception( Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, true );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.RemoteRuntimeError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty.And.No.StringContaining( exception.Message ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.DebugInformationKeyUtf8 ].AsString(), Is.StringContaining( exception.Message ) );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_NotNull_OtherException_IsNotDebugMode_AsRemoteRuntimeError()
+		{
+			var exception = new Exception( Guid.NewGuid().ToString() );
+
+			var result = InvocationHelper.HandleInvocationException( exception, false );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.RemoteRuntimeError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty.And.No.StringContaining( exception.Message ) );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_Null_IsDebugMode_AsRemoteRuntimeError()
+		{
+			var result = InvocationHelper.HandleInvocationException( null, false );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.RemoteRuntimeError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+
+		[Test()]
+		public void TestHandleInvocationException_Null_IsNotDebugMode_AsRemoteRuntimeError()
+		{
+			var result = InvocationHelper.HandleInvocationException( null, false );
+
+			Assert.That( result.Error, Is.EqualTo( RpcError.RemoteRuntimeError ) );
+			Assert.That( result.Detail.AsDictionary()[ RpcException.MessageKeyUtf8 ].AsString(), Is.Not.Null.And.Not.Empty );
+			Assert.That( result.Detail.AsDictionary().ContainsKey( RpcException.DebugInformationKeyUtf8 ), Is.False );
+		}
+	}
+}
