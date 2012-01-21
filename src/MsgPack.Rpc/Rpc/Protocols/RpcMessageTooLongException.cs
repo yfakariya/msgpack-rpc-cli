80a03ddb786736ac1c00d83e22c688b9fa00b7f6
yfakariya <yfakariya@gmail.com>
2012-01-22 03:20:57 +0900

    Fix message sometimes wrong.

--- src/MsgPack.Rpc/Rpc/Protocols/RpcMessageTooLongException.cs
+++ src/MsgPack.Rpc/Rpc/Protocols/RpcMessageTooLongException.cs
@@ -34,7 +34,7 @@ namespace MsgPack.Rpc.Protocols
 		/// <summary>
 		///		Initializes a new instance of the <see cref="RpcMessageTooLongException"/> class with the default error message.
 		/// </summary>
-		public RpcMessageTooLongException() : this( "Message is too large.", null ) { }
+		public RpcMessageTooLongException() : this( null, null ) { }
 
 		/// <summary>
 		///		Initializes a new instance of the <see cref="RpcMessageTooLongException"/> class with a specified error message.
@@ -46,7 +46,7 @@ namespace MsgPack.Rpc.Protocols
 		///		Debug information of error.
 		///		This value can be null for security reason, and its contents are for developers, not end users.
 		/// </param>
-		public RpcMessageTooLongException( string message, string debugInformation ) : base( RpcError.MessageTooLargeError, message, debugInformation ) { }
+		public RpcMessageTooLongException( string message, string debugInformation ) : this( message, debugInformation, null ) { }
 
 		/// <summary>
 		///		Initializes a new instance of the <see cref="RpcMessageTooLongException"/> class with a specified error message and a reference to the inner exception that is the cause of this exception. 
